7bfc27c8de6f7d692c7685a4d485eb01e0fe52ab
Karan Bagle <karanbagle420@GMAIL.COM>
2024-10-21 18:20:31 -0700

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,15 @@
+import React from 'react';
+
 function Dashboard() {
+  console.log('Dashboard component is rendering');
+
   return (
     <div>
       <h1>Welcome to PDF to Word Converter</h1>
     </div>
   );
 }
+
 
 
 export default Dashboard;
